603e1b8b6921b5fb8691383f83d1fe0933651f24
acharya-pooja57 <122407972+acharya-pooja57@users.noreply.github.com>
2023-02-06 22:59:42 -0500

    Add files via upload

---
+++ assignment2.sh
@@ -0,0 +1,8 @@
+#!/bin?bash 
+echo "This script can count the number of sequecnes in a fasta file." 
+greeting="Hello" yourname="Pooja" 
+echo $greeting $yourname! 
+#mkdir /Users/poojaacharya/Desktop/learning_bash 
+#mkdir /Users/poojaacharya/Desktop/learning_bash/bioinformatics 
+cp /Users/poojaacharya/Desktop/command_line/transcriptome.fasta /Users/poojaacharya/Desktop/learning_bash/bioinformatics 
+grep -c ">" transcriptome.fasta >> fasta_count.txt
