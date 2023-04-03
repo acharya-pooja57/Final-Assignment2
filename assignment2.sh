5f961a7e29487b5f57aabe87142c096394ff8572
acharya-pooja57 <122407972+acharya-pooja57@users.noreply.github.com>
2023-04-03 11:49:22 -0400

    Update assignment2.sh

--- assignment2.sh
+++ assignment2.sh
@@ -5,4 +5,5 @@ echo $greeting $yourname!
 #mkdir /Users/poojaacharya/Desktop/learning_bash 
 #mkdir /Users/poojaacharya/Desktop/learning_bash/bioinformatics 
 cp /Users/poojaacharya/Desktop/command_line/transcriptome.fasta /Users/poojaacharya/Desktop/learning_bash/bioinformatics 
+cd /Users/poojaacharya/Desktop
 grep -c ">" transcriptome.fasta >> fasta_count.txt
